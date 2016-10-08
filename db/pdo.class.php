2b21d3b4e90f5e47fe921c1256f604d353bea3ba
XXF92 <903004228@qq.com>
2016-10-08 14:31:42 +0800

    xxf-111111-----mysql class

---
+++ db/pdo.class.php
@@ -0,0 +1,24 @@
+<?php
+class pdo implements \Git\db\IDatabase
+{
+    function connect()
+    {
+        // TODO: Implement connect() method.
+        echo "xf----------------1";
+    }
+
+    function query()
+    {
+        // TODO: Implement query() method.
+        echo "xf----------------1";
+    }
+
+    function close()
+    {
+        // TODO: Implement close() method.
+        echo "xf----------------1";
+    }
+
+
+
+}
