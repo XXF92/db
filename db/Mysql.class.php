a441c81b6eef1a654305d4565960c369785528f0
XXF92 <903004228@qq.com>
2016-10-08 14:01:51 +0800

    xxf------mysql class

--- db/Mysql.class.php
+++ db/Mysql.class.php
@@ -5,15 +5,18 @@ class Mysql implements IDatabase
     function connect()
     {
         // TODO: Implement connect() method.
+        echo "database connect";
     }
 
     function query()
     {
         // TODO: Implement query() method.
+        echo "database query";
     }
 
     function close()
     {
         // TODO: Implement close() method.
+        echo "database close";
     }
 }
